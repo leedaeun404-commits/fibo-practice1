ab277a11e3c14e644b52346031630c40b55d17a6
leedaeun404-commits <leedaeun404-commits@users.noreply.github.com>
2025-12-29 15:51:26 +0900

    feat: add fibonacci example

---
+++ fibo-first.py
@@ -0,0 +1,13 @@
+def fibo(n):
+    if n < 2:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+if __name__ == "__main__":
+    print(fibo(10))def fibo(n):
+    if n < 2:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+if __name__ == "__main__":
+    print(fibo(10))
